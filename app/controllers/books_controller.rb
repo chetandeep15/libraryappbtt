d9c2909c578274f8e9002e92b29e8fbbf9658c52
chetandeep singh pahuja <chetan.deep24@gmail.com>
2016-05-28 03:33:32 +0530

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,11 @@ class BooksController < ApplicationController
 	def index
     @book = Book.all
   end
-def show
-    @book = Book.find(params[:id])
-end
+
 def new
  @book = Book.new
 end
-def edit
-  @book = Book.find(params[:id])
-end
+
 def create
   @book = Book.new(book_params)
  
