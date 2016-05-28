70d76341f9832154781b1944a6b696a9ffc23360
chetandeep singh pahuja <chetan.deep24@gmail.com>
2016-05-29 03:40:04 +0530

    Update sessions_controller_test.rb

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,5 +6,16 @@ class SessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
   
+  test "should_get_login_form" do
+  	get :new
+  	assert_template :new
+  end
+
+  test "should_login" do
+  	post :create, session: { email: 'example@valid.com', password: 'password' }
+  	assert session[:user_id].nil?
+  	
+  end
+  
 
 end
